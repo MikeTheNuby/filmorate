155a6547390d7707298529f1be21a2bf50fccc6c
Nuby <nuby.4register@gmail.com>
2023-04-09 22:37:36 +0300

    Fix: annotations Users

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -4,10 +4,7 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
@@ -19,11 +16,11 @@ public class User {
     @Min(value = 0, message = "Значение id не может быть отрицательным.")
     private long id;
     private String name;
-    @NotNull(message = "Логин не может быть пустым.")
+    @NotBlank(message = "Логин не может быть пустым.")
     private String login;
     @Email(message = "Формат @mail не соответствует требованиям.")
     private String email;
-    @NotNull(message = "Дата рождения не может быть пустой.")
+    @NotBlank(message = "Дата рождения не может быть пустой.")
     @Past(message = "Дата рождения не может быть в будущем времени.")
     private LocalDate birthday;
     private final Set<Long> friends = new HashSet<>();
