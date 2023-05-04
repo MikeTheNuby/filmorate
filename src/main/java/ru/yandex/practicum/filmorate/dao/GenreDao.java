e118777b4e6e3a68607b8d8f43061d22eb94d073
Nuby <nuby.4register@gmail.com>
2023-05-04 09:48:26 +0300

    add: dao package and DAO interface for Friendship, Genre and MPA rating.

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/GenreDao.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.dao;
+
+import ru.yandex.practicum.filmorate.model.Genre;
+
+import java.util.List;
+
+public interface GenreDao {
+
+    List<Genre> getGenres();
+
+    Genre findGenreById(int id);
+
+    List<Genre> getGenresByFilm(long id);
+}
